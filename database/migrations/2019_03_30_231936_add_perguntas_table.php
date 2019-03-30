657a3ece241b60aef7d8ac34c804942c982defc6
Murielson <jhonsnoow32@gmail.com>
2019-03-30 18:24:28 -0500

    #5 Tabela de perguntas criada

---
+++ database/migrations/2019_03_30_231936_add_perguntas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPerguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perguntas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->mediumText('texto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perguntas');
+    }
+}
