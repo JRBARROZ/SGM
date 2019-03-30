bdd60031eef08f7e8e2e85836d34ad6736d70e2b
Murielson <jhonsnoow32@gmail.com>
2019-03-30 18:33:29 -0500

    #5 Chaves estrangeiras : (perguntas) and (respostas) adicionadas a tabela votos

---
+++ database/migrations/2019_03_30_232614_add_perguntas_respostas_id_on_votos.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPerguntasRespostasIdOnVotos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('votos', function (Blueprint $table) {
+            //Create perguntas and respostas foreign key.
+
+            //Foreign-key --> (Perguntas) <--
+            $table->integer('perguntas_id')->unsigned();
+            $table->foreign('perguntas_id')->references('id')->on('perguntas');
+            
+            //Foreign-key --> (Respostas) <--
+            $table->integer('respostas_id')->unsigned();
+            $table->foreign('respostas_id')->references('id')->on('respostas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('votos', function (Blueprint $table) {
+            //Drop perguntas and cadeiras foreign key, and column.
+            $table->dropForeign(['perguntas_id'], ['respostas_id']);
+            $table->dropColumns(['pergunstas_id'], ['respostas_id']);
+        });
+    }
+}
