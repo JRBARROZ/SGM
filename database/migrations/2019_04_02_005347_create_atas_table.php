48e60d1b714b00804f52b15144a4646f441762de
Murielson <jhonsnoow32@gmail.com>
2019-04-01 22:37:31 -0500

    #20 Implementando tabela de atas

---
+++ database/migrations/2019_04_02_005347_create_atas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atas');
+    }
+}
