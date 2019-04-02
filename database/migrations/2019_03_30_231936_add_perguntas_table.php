6c69e959de61b52dd7756040b55da580f331ae8c
eduardobispof <eduardobispof@gmail.com>
2019-04-02 13:01:38 -0300

    adicionando listagem de perguntas cebosa

--- database/migrations/2019_03_30_231936_add_perguntas_table.php
+++ database/migrations/2019_03_30_231936_add_perguntas_table.php
@@ -16,6 +16,7 @@ class AddPerguntasTable extends Migration
         Schema::create('perguntas', function (Blueprint $table) {
             $table->increments('id');
             $table->string('titulo');
+            $table->enum('estado', ['aberta', 'respondida'])->default('aberta');
             $table->mediumText('texto');
         });
     }
