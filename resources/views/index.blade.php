98be7c049fdd5816a73f0d67f587179cc8d6fccc
josecarlosmonteiro <1monteirocarlos@gmail.com>
2019-04-06 03:44:50 -0300

    reformulação da tela index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,70 +1,73 @@
 @extends('layouts.app')
 
+
 @section('content')
-	<div class="jumbotron" style="padding: 20px;">
+	<div class="jumbotron" style="padding: 20px; width: 100%;">
 		<div class="container text-center">
-			<h2>Sistema de Gerenciamento de Monitorias</h2>
-			<h5>Feito por alunos para auxiliar alunos!</h5>
+			<h2 class="text-success">Sistema de Gerenciamento de Monitorias</h2>
+			<h5 class="text-secondary">Feito por alunos para auxiliar alunos!</h5>
 		</div>
 	</div>
-
 	<div class="container">
 		<div class="row justify-content-center">
-			<div class="col-md-10 border rounded">
-				<br>
-				<h3 class="ml-3">Explore nossas perguntas
-					<form action="" class="float-right form-inline mr-3">
-						<label class="h5 mr-2">Filtrar postagens: </label>
-						<input type="text" class="form-control d-inline mr-2" placeholder="Palavra, tag ou curso...">
-						<button type="submit" class="btn btn-success">Buscar</button>
+			<div class="col-md-8">
+				@auth
+					<form method="POST" action="{{route('adicionar-pergunta')}}" class="form-group">
+						@csrf
+						
+						<label for="">Titulo da pergunta</label>
+						<input type="text" name="titulo" maxlength="200" class="form-control">
+						<label for="">Descrição da pergunta:</label>
+						<textarea class="form-control" name="descricao" aria-label="With textarea" placeholder="descreva aqui sua dúvida"></textarea>
+						<label for="">Curso:</label>
+						<select class="form-control" name="curso">
+							@foreach ($cursos as $curso)
+								<option value="{{$curso->id}}"> {{$curso->nome}} - ({{$curso->sigla}})</option>
+							@endforeach
+						</select>
+						<br>
+						<button type="submit" class="btn btn-success">postar</button>
 					</form>
-				</h3>
-				<hr>
-				<div id="posts">
-					<div id="post-info" class="border-bottom">
-						<a href="#" class="text-dark">
-							<h4 class="d-inline">Título da postagem</h4>
-						</a>
-							<span class="text-secondary ml-2">Postado em 00/00/0000</span>
-							<span id="curso" class="badge badge-secondary float-right">LOG</span>
-							<span id="estado" class="badge badge-info float-right mr-2">Aberto</span>
-							<h6>
-								<a href="#" class="badge badge-secondary">tag</a>
-								<a href="#" class="badge badge-secondary">tag</a>
-								<a href="#" class="badge badge-secondary">tag</a>
-							</h6>
-					</div>
-					<div id="post-info" class="border-bottom">
-						<a href="#" class="text-dark">
-							<h4 class="d-inline">Título da postagem</h4>
-						</a>
-							<span class="text-secondary ml-2">Postado em 00/00/0000</span>
-							<span id="curso" class="badge badge-secondary float-right">IPI</span>
-							<span id="estado" class="badge badge-success float-right mr-2">Resolvido</span>
-							<h6>
-								<a href="#" class="badge badge-secondary">tag</a>
-								<a href="#" class="badge badge-secondary">tag</a>
-								<a href="#" class="badge badge-secondary">tag</a>
-							</h6>
-					</div>
-					<div id="post-info" class="border-bottom">
-						<a href="#" class="text-dark">
-							<h4 class="d-inline">Título da postagem</h4>
-						</a>
-							<span class="text-secondary ml-2">Postado em 00/00/0000</span>
-							<span id="curso" class="badge badge-secondary float-right">LOG</span>
-							<span id="estado" class="badge badge-info float-right mr-2">Aberto</span>
-							<h6>
-								<a href="#" class="badge badge-secondary">tag</a>
-								<a href="#" class="badge badge-secondary">tag</a>
-								<a href="#" class="badge badge-secondary">tag</a>
-							</h6>
-					</div>
-				</div>
+				@endauth
+
+				<table class="table" style="box-shadow: 0px 0px 10px #dadbdc;">
+					<thead class="bg-success">
+						<tr class="text-center">
+							<th><h3 class="text-light">Expore nossas postagens</h3></th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($perguntas as $pergunta)
+							<tr>
+								<td>
+									<div class="m-3">
+										<a href="{{route('exibir-pergunta', $pergunta->id)}}" class="h4 text-dark">{{$pergunta->titulo}}</a>
+										<span class="badge badge-secondary  float-right mr-2">{{$pergunta->cursos[0]->sigla}}</span>
+										@if($pergunta->estado == "Aberto")
+											<span class="badge badge-info text-light float-right mr-2">{{$pergunta->estado}}</span>
+										@else
+											<span class="badge badge-success text-light float-right mr-2">{{$pergunta->estado}}</span>
+										@endif
+										<span class="h6 text-secondary d-block mt-2" style="font-size: 12px;">{{$pergunta->created_at}}</span>
+										<h6 class="mt-3">
+											<a href="#" class="text-success mr-1">tag</a>
+											<a href="#" class="text-success mr-1">tag</a>
+											<a href="#" class="text-success mr-1">tag</a>
+											@if($pergunta->users_id == Auth::id())
+												<a href="#" class="float-right mr-2 text-danger"><span>Deletar</span></a>
+												<a href="#" class="float-right mr-2 text-info"><span>Editar</span></a>
+											@endif
+										</h6>
+									</div>
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
-@stop
+@endsection
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
