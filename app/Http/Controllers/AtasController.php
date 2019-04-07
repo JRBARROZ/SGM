af906916235e015c939e43df0412abf97f3cab2d
Murielson <jhonsnoow32@gmail.com>
2019-04-07 16:51:59 -0500

    #20 Criando controller de atas

--- app/Http/Controllers/AtasController.php
+++ app/Http/Controllers/AtasController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Ata;
+use App\Curso;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 
@@ -17,9 +18,19 @@ class AtasController extends Controller
      */
     public function index()
     {   
-        $monitor = User::find(Auth::id());
-        $alunos = User::where('tipo', 'aluno')->where('fk_curso', $monitor->fk_curso)->get();
-        return view('ata', compact('alunos', 'monitor'));
+        
+        //Recebe e verifica se o usuário é aluno ou monitor.
+        $privilegio = User::find(Auth::id());
+        if($privilegio->tipo == 'aluno'){
+            return redirect('/');
+        }else{
+            $cursos = User::find(Auth::id());
+            $sigla = $cursos->cursos;
+            //Pegando o curso e a sigla
+            $alunos = User::where('tipo', 'aluno')->where('fk_curso', $privilegio->fk_curso)->get();
+            return view('ata', compact('alunos', 'privilegio', 'sigla'));
+        }
+        
     }
     /**
      * Store a newly created resource in storage.
@@ -28,9 +39,23 @@ class AtasController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        $myCheckboxes = Input::get('options');
-        var_dump($myCheckboxes);
+    {   
+        //Aqui recebo os nomes dos usuarios presentes.
+        $value = $request->get('value');
+        $request->data;
+        var_dump($value);
+
+        // Pegando SIGLA
+        $cursos = User::find(Auth::id());
+        $sigla = $cursos->cursos;
+        $ata = new Ata();
+        for($i=0; $i < sizeof($value);$i++){ 
+             $ata->nome = $value[$i];
+             $ata->curso = $sigla[0]['sigla'];
+             $ata->id_user = Auth::id();
+             $ata->id_curso = $sigla[0]['id'];
+             $ata->save();
+        }
     }
 
     /**
