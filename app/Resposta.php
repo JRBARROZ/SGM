0204ce8f126b00ff5cb9648697ef2c9d30bc7623
eduardobispof <eduardobispof@gmail.com>
2019-04-08 09:01:50 -0300

    criando RespostaController sem funções

--- app/Resposta.php
+++ app/Resposta.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resposta extends Model
 {
-    //
+	protected $table = 'perguntas';
+
+    protected $fillable = [
+        'titulo', 'texto', 'users_id', 'estado'
+    ];
+
+    public function pergunta()
+    {
+        return $this->hasMany('App\Pergunta', 'id', 'perguntas_id');
+    }
+
 }
