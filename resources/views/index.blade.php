45473c6a4a37655a24731f77f875a23a827bb7bd
eduardobispof <eduardobispof@gmail.com>
2019-04-08 16:59:03 -0300

    removendo respontas junto com perguntas no botão de deletar perguntas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -63,7 +63,7 @@
 											<a href="#" class="text-success mr-1">tag</a>
 											<a href="#" class="text-success mr-1">tag</a>
 											@if($pergunta->users_id == Auth::id())
-												<a href="#" class="float-right mr-2 text-danger"><span>Apagar</span></a>
+												<a href="{{ route('delete', $pergunta->id) }}" class="float-right mr-2 text-danger"><span>Apagar</span></a>
 												<a href="{{route('editar-pergunta', $pergunta->id)}}}" class="float-right mr-2 text-info"><span>Editar</span></a>
 											@endif
 										</h6>
