79440408fff37142d7a2a9ad8132b1eae0c5f611
Murielson <jhonsnoow32@gmail.com>
2019-04-13 21:45:47 -0500

    #20 Controller de atas

---
+++ app/Http/Controllers/AtaController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Curso;
+use App\Cadeira;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Ata;
+
+class AtaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuario = User::find(Auth::id());
+        $alunos = User::where('tipo', 'aluno')->where('fk_curso', $usuario->fk_curso)->get();
+        $orientador = User::where('tipo', 'professor')->where('fk_curso', $usuario->fk_curso)->get();
+        $curso = Curso::where('id', $usuario->fk_curso)->get();
+        $cadeira = Cadeira::where('id', $usuario->cadeira_id)->get();
+        if($usuario->tipo == 'aluno'){
+            return redirect('/');
+        }else{
+            return view('ata', compact('usuario', 'alunos', 'curso', 'orientador', 'cadeira'));
+        }
+        return view('ata');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $monitor = User::find(Auth::id());
+        $users = $request->get('presente');
+        $cadeira = Cadeira::where('id', $monitor->cadeira_id)->get();
+        // var_dump($cadeira[0]['fk_curso']);
+        foreach($users as $item){
+            $ata = new Ata();
+            $ata->texto = $request->message;
+            $ata->user_id = $item;
+            $ata->curso_id = $monitor->fk_curso;
+            $ata->cadeira_id = $cadeira[0]['id'];
+            $ata->save();
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
