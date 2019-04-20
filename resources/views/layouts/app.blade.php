01b25a29ef61595e387e9596f7bb1cd9a286d417
Silvio <silvioej@gmail.com>
2019-04-20 19:39:54 -0300

    #36 link para redirecionar a pagina do perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,8 +51,12 @@
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
+
                         <div id='app' class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            <a class="dropdown-item" href="{{ route('perfil') }}">
+                            {{ __('Perfil') }}
+                            </a>
+                             <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
