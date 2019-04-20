b80a7e637b4eafe2106471e26254474da4d68f67
Murielson <jhonsnoow32@gmail.com>
2019-04-20 18:39:14 -0500

    #36 adicionando view do user

--- resources/views/user/painelHome.blade.php
+++ resources/views/user/painelHome.blade.php
@@ -1,1 +1,13 @@
 @extends('layouts.dashboard', ['current'=>'home'])
+
+@section('content')
+    <div class="row user-img"></div>
+    <div class="row">
+        <div class="col-12 text-center">
+            <img src="{{asset('imagens/marsh.jpg')}}" alt="..." class="img-thumbnail rounded-circle user-profile-img">
+            <h2>USERNAME</h2>
+            <h3 class="text-secondary"><i>Monitor de (Cadeira)</i></h3>         
+        </div>
+    </div>
+    <hr>
+@endsection
