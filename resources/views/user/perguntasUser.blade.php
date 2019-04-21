c6f9dcbb2be51340146b1f83a81d0e3377d96b98
Murielson <jhonsnoow32@gmail.com>
2019-04-21 16:49:37 -0500

    #36 adicionando view de listagem de perguntas realizadas pelo usuário

---
+++ resources/views/user/perguntasUser.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard', ['current'=>'perguntasUser'])
+
+@section('content')
+    <div class="breadcrumb" style="border:none;background-color:white;">
+        <h1>Tópicos</h1>
+    </div>
+@endsection
