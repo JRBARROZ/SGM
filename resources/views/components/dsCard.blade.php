fa99d982611f2827327cb7daf353d55f96cccf14
Murielson <jhonsnoow32@gmail.com>
2019-04-21 16:49:06 -0500

    #36 adicionando template dos cards do perfil do user

---
+++ resources/views/components/dsCard.blade.php
@@ -0,0 +1,11 @@
+<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 slide-fwd-center">
+    <a href="{{$slot}}" class="text-dark">        
+        <div class="box-card text-center">                            
+            <div class="title">
+                <h4>{{ $nome }}</h4>
+            </div>
+            <h2>{{ $valor }}</h2>
+            <a href="#">Detalhes</a>
+        </div>
+    </a>
+</div>
