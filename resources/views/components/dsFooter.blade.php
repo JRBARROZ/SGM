14a6af8212ea9d846b0151cb210d5719225e45e4
Murielson <jhonsnoow32@gmail.com>
2019-04-21 16:24:36 -0500

    #36 Ajustando cores do footer e melhorando a descrição

--- resources/views/components/dsFooter.blade.php
+++ resources/views/components/dsFooter.blade.php
@@ -1,4 +1,4 @@
-<footer class="sticky-footer">
+<footer class="sticky-footer" style="background:	#E0E0E0;">
     <div class="container">
         <div class="text-center">
             <small>Copyright SGM @2019</small> 
