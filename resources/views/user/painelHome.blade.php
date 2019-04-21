40480b2d762607fd6b3171bd3d7e1119cc131d15
Murielson <jhonsnoow32@gmail.com>
2019-04-21 16:39:49 -0500

    #36 Adicionando cards-links para listagem de votos,tópicos e tópicos resolvidos

--- resources/views/user/painelHome.blade.php
+++ resources/views/user/painelHome.blade.php
@@ -4,10 +4,53 @@
     <div class="row user-img"></div>
     <div class="row">
         <div class="col-12 text-center">
-            <img src="{{asset('imagens/marsh.jpg')}}" alt="..." class="img-thumbnail rounded-circle user-profile-img">
-            <h2>USERNAME</h2>
-            <h3 class="text-secondary"><i>Monitor de (Cadeira)</i></h3>         
+            <img src="{{asset($user->avatar)}}" alt="..." class="img-thumbnail rounded-circle user-profile-img">
+            <h2>{{Auth::user()->name}} {{Auth::user()->sobrenome}}</h2>
+            @switch(Auth::user()->tipo)
+                @case('monitor')
+                    <h3 class="text-secondary"><i>Monitor de (Redes de Computadores)</i></h3>          
+                    @break
+                @case('aluno')
+                    @foreach($user->cursos as $item)
+                        <h3 class="text-secondary"><i>Aluno do {{Auth::user()->periodo}}° Periodo - {{$item->sigla}}  </i></h3>          
+                    @endforeach
+                    @break
+                @default
+                    <h3 class="text-secondary"><i>Staff</i></h3>          
+            @endswitch
         </div>
     </div>
     <hr>
+    <br>
+    <div class="row">
+        {{-- Tópicos Fórum --}}
+        @component('components.dsCard')
+            @slot('nome')
+                Tópicos no Fórum
+            @endslot 
+            @slot('valor')
+                {{sizeof($perguntas)}}
+            @endslot
+            {{route('user.create')}}
+        @endcomponent
+        {{-- Tópicos Resolvidos --}}
+        @component('components.dsCard')
+            @slot('nome')
+                Tópicos Resolvidos
+            @endslot 
+            @slot('valor')
+                0
+            @endslot
+        @endcomponent
+        {{-- Votos --}}
+        @component('components.dsCard')
+            @slot('nome')
+                Votos
+            @endslot 
+            @slot('valor')
+                0
+            @endslot
+        @endcomponent
+    </div>
+    <br><br>
 @endsection
