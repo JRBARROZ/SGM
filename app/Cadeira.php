648d0dc8f1949fdf1f2b5760847028eeb601222c
eduardobispof <eduardobispof@gmail.com>
2019-04-21 20:54:27 -0300

    automatisando UserFactory

--- app/Cadeira.php
+++ app/Cadeira.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cadeira extends Model
 {
-    //
+	protected $table = 'cadeiras';
+
+    protected $fillable = [
+        'nome', 'periodo', 'fk_curso'
+    ];
+
 }
