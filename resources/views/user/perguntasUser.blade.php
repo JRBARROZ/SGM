8f65817707465ff1df64d95d1b05fb1651939326
Murielson <jhonsnoow32@gmail.com>
2019-04-21 20:08:45 -0500

    #36 Finalizando listagem de perguntas

--- resources/views/user/perguntasUser.blade.php
+++ resources/views/user/perguntasUser.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.dashboard', ['current'=>'perguntasUser'])
 
 @section('content')
-    <div class="breadcrumb" style="border:none;background-color:white;">
-        <h1>Tópicos</h1>
+<div class="card bg-dark" style="padding:10px;border:none;margin-top:10px;">
+    <div class="row">
+        @foreach ($perguntas as $item)
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 slide-fwd-center" style="box-shadow:none;">
+                <a href="{{route('exibir-pergunta', $item->id)}}" class="text-dark">        
+                    <div class="box-card text-center">                            
+                        <div class="title">
+                            <h5>{{$item->titulo}}</h5>
+                            <p class="text-secondary"><i>{{$data}} - {{Auth::user()->name}}</i></p>
+                        </div>
+                        <h2></h2>
+                        <a href="{{route('exibir-pergunta', $item->id)}}">Ver Discussão >></a>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
     </div>
 @endsection
