3e31b9d93519ebc3db618b80894916d4666f0457
eduardobispof <eduardobispof@gmail.com>
2019-04-22 10:57:11 -0300

    removendo formulario de agendamento de monitoria da pagina de listagem

--- resources/views/monitoria.blade.php
+++ resources/views/monitoria.blade.php
@@ -4,35 +4,6 @@
 
 	<div class="container" style="padding: 20px; width: 100%;">
 		<div class="row justify-content-center">
-			@auth
-				@if(Auth::user()->tipo == "monitor")
-					<div class="col-md-3 col-6 mr-3 mb-5 mt-0 p-0">
-						<h3>Agendar Monitoria</h3>
-						<form action="">
-							<label for="">
-								Titulo da atividade
-								<input type="text" class="form-group form-control" name="titulo" placeholder="assunto da monitoria">
-								Descrição da atividade
-								<textarea class="form-group form-control" name="descricao" placeholder="descrição da monitoria"></textarea>
-							</label>
-
-							<label for="">
-								Horário
-								inicio:
-								<input class="form-group form-control" type="time" name="hora_inicio">
-								termino:
-								<input class="form-group form-control" type="time" name="hora_termino">
-							</label>
-							<label for="">
-								Data
-								<input type="date" name="data" class="form-group form-control">
-							</label>
-							<br>
-							<button class="btn btn-success">Salvar</button>
-						</form>
-					</div>
-				@endif
-			@endauth
 			<div class="col-md-8 mb-5">
 				<table>
 					<tr>
@@ -53,7 +24,7 @@
 				<table class="table table-bordered">
 					<thead class="bg-success">
 						<tr align="center">
-							<th colspan="4">Monitorias agendadas por você</th>
+							<th colspan="4">Monitorias agendadas para você</th>
 						</tr>
 					</thead>
 					<tbody>
