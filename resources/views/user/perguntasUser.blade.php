b40175e79067804e3369cb039010faca31e60162
Murielson <jhonsnoow32@gmail.com>
2019-04-24 21:55:25 -0500

    #37 Melhorando listagem de tópicos

--- resources/views/user/perguntasUser.blade.php
+++ resources/views/user/perguntasUser.blade.php
@@ -1,22 +1,31 @@
-@extends('layouts.dashboard', ['current'=>'perguntasUser'])
+@extends('layouts.app', ['current'=>'perguntasUser'])
 
 @section('content')
-<div class="card bg-dark" style="padding:10px;border:none;margin-top:10px;">
-    <div class="row">
-        @foreach ($perguntas as $item)
-        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 slide-fwd-center" style="box-shadow:none;">
-                <a href="{{route('exibir-pergunta', $item->id)}}" class="text-dark">        
-                    <div class="box-card text-center">                            
-                        <div class="title">
-                            <h5>{{$item->titulo}}</h5>
-                            <p class="text-secondary"><i>{{$data}} - {{Auth::user()->name}}</i></p>
-                        </div>
-                        <h2></h2>
-                        <a href="{{route('exibir-pergunta', $item->id)}}">Ver Discussão >></a>
-                    </div>
-                </a>
-            </div>
-            @endforeach
-        </div>
+<div class="container">
+    <div class="title bg-success text-center text-white" style="margin-top:3%;padding:10px;">
+        <h2>Seus Tópicos</h2>
     </div>
+    <br>
+    @if(sizeof($perguntas) == 0)
+    <h2 class="text-dark">Parece que você não tem tópicos no nosso fórum :/</h2>    
+    <div class="row">
+        @else
+        @foreach ($perguntas as $item)
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 slide-fwd-center">
+            <a href="{{route('exibir-pergunta', $item->id)}}" class="text-dark">        
+                <div class="box-card text-center">                            
+                    <div class="title">
+                        <h5>{{$item->titulo}}</h5>
+                        <p class="text-secondary"><i>{{$data}} - {{Auth::user()->name}}</i></p>
+                    </div>
+                    <h2></h2>
+                    <a href="{{route('exibir-pergunta', $item->id)}}">Ver Discussão >></a>
+                </div>
+            </a>
+        </div>
+        
+        @endforeach
+    </div>
+    @endif
+</div>
 @endsection
