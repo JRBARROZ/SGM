905a017b63dc58b8ab536055c58c34bc4f46a2da
Murielson <jhonsnoow32@gmail.com>
2019-04-24 21:21:16 -0500

    #36 Mix de atualização do js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,3 +12,5 @@ const mix = require('laravel-mix');
  */
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+// Mixando Css do user!   
+mix.sass('resources/sass/user.scss', 'public/css');   
