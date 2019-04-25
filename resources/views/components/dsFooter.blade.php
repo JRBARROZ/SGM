337254b0dd60d93d25e5f2d9ed347263d18f7228
FernandaBatistaVS <pontocidadaofernanda@gmail.com>
2019-04-24 22:47:06 -0300

    #35 criando o footer

--- resources/views/components/dsFooter.blade.php
+++ resources/views/components/dsFooter.blade.php
@@ -1,7 +1,51 @@
-<footer class="sticky-footer" style="background:	#E0E0E0;">
-    <div class="container">
-        <div class="text-center">
-            <small>Copyright SGM @2019</small> 
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/Footer-with-logo.css">
+</head>
+
+<body>
+<div class="content">
+</div>
+    <footer id="myFooter" class=" container-fluid bg-success">
+            <div class="row justify-content-between">
+                <div class="col-sm">
+                    <h5>Vamos começar</h5>
+                    <ul>
+                        <li><a href="#" class="text-light">Página Inicial</a></li>
+                        <li><a href="#" class="text-light">Entrar</a></li>
+                        <li><a href="#" class="text-light">Cadastrar</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm">
+                    <h5>Sobre o IFPE</h5>
+                    <ul>
+                        <li><a href="#" class="text-light">Informações</a></li>
+                        <li><a href="#" class="text-light">Valores</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm">
+                    <h5>Suporte</h5>
+                    <ul>
+                        <li><a href="#" class="text-light">FAQ</a></li>
+                        <li><a href="#" class="text-light">Ajuda</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm info">
+                    <h5 class="text-light">Informações</h5>
+                    <p class="text-light"> O projeto SGM tem como objetivo facilitar o método de integração de monitorias com os alunos nos campus IFPE. Com o intuito de apresentar uma plantaforma inovadora, rápida e dinâmica. </p>
+                </div>
+            </div>
         </div>
-    </div>
-</footer>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
