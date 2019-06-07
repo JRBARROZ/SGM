8578ddd0fbd76bc522663aeab0145a92f9ddcd0f
Murielson <jhonsnoow32@gmail.com>
2019-06-06 23:27:53 -0300

    #65 adaptando view de admin e retirando botões desnecessários

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,20 +6,19 @@
 		<div class="row">
 			<div class="col-md">
 				<div class="container">
-					<h3 class="text-center mt-4 mb-5">Dashboard Administrador</h3>
-
+					<br>
 					<div class="row justify-content-center">
 						<div class="col-lg-4">
 							<div class="m-2 border text-center">
-								<button class="btn btn-light btn-block text-danger" data-toggle="collapse" data-target="#reclamacoes">
-									<h3>Reclamações</h3>
+								<button class="btn btn-light btn-block text-dark" data-toggle="collapse" data-target="#reclamacoes">
+									<h3>Atas</h3>
 									<strong class="h4">7</strong>
 								</button>
 							</div>
 						</div>
 						<div class="col-lg-4">
 							<div class="m-2 border text-center">
-								<button class="btn btn-light btn-block text-primary" data-toggle="collapse" data-target="#atas">
+								<button class="btn btn-light btn-block text-dark" data-toggle="collapse" data-target="#atas">
 									<h3>Relatórios</h3>
 									<strong class="h4">15</strong>
 								</button>
@@ -27,9 +26,9 @@
 						</div>
 						<div class="col-lg-4">
 							<div class="m-2 border text-center">
-								<button class="btn btn-light btn-block text-info" data-toggle="collapse" data-target="#monitores">
+								<button class="btn btn-light btn-block text-dark" data-toggle="collapse" data-target="#monitores">
 									<h3>Listar Monitores</h3>
-									<strong class="h4">18</strong>
+									<strong class="h4">{{$totalMonitores}}</strong>
 								</button>
 							</div>
 						</div>
@@ -40,9 +39,9 @@
 						<!--Div com lista de reclamações/denúncias feitas pelos usuários-->
 						<div id="reclamacoes" class="col-12 collapse">
 							<div class="m-2">
-								<table class="table table-bordered table-hover shadow">
+								<table class="table table-borderless table-hover">
 									<thead class="text-center">
-										<tr class="text-light bg-danger text-center">
+										<tr class="text-light bg-success">
 											<th>Postagem</th>
 											<th>Ações</th>
 										</tr>
@@ -67,9 +66,9 @@
 						<!--Listar arquivos de relatórios entregues-->
 						<div id="atas" class="col-12 collapse">
 							<div class="m-2">
-								<table class="table table-hover table-bordered shadow">
+								<table class="table table-hover table-borderless ">
 									<thead class="text-center">
-										<tr class="bg-primary text-light">
+										<tr class="bg-success text-light">
 											<th>Monitor</th>
 											<th>Arquivo</th>
 											<th>Curso</th>
@@ -103,9 +102,9 @@
 								<div class="mt-4 mb-4">
 									<input id="filtroMonitores" type="text" class="form-control" placeholder="Filtrar por nome...">
 								</div>
-								<table class="table table-bordered table-hover shadow">
-									<thead class="text-center">
-										<tr class="text-light bg-info">
+								<table class="table table-borderless table-hover">
+									<thead>
+										<tr class="text-light bg-success">
 											<th>Nome do Monitor</th>
 											<th>Curso</th>
 											<th>Cadeira</th>
@@ -123,14 +122,12 @@
                                                     <td>{{$cadeira->periodo}}</td>
                                                     <td>
                                                         <div class="row justify-content-center">
-                                                            <a href="#" class="m-2 text-primary">Visitar Perfil</a>
-                                                            <a href="#" class="m-2 text-danger">Alterar Conta</a>
+                                                            <a href="{{route('user.show', ['id' => $cadeira->id])}}" class="m-2 text-primary">Visitar Perfil</a>
                                                         </div>
                                                     </td>
                                                 </tr>
                                             @endforeach
                                         @endforeach
-                                        {{$monitores->links()}}
 									</tbody>
 								</table>
 							</div>
@@ -142,9 +139,9 @@
 							<h3 class="mb-2">Pendências</h3>
 							<br>
 							<div class="row">
-								<table class="table table-hover table-bordered shadow">
+								<table class="table table-hover table-borderless ">
 									<thead class="text-center">
-										<tr class="bg-dark text-success">
+										<tr class="bg-success text-white">
 											<th>Nome</th>
 											<th>Cadeira</th>
 											<th>Mês</th>
